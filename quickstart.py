f4da3cfd62f53171640e6e06e463507123dbaa04
Ignacio Castañeda <icastanedaw@gmail.com>
2019-01-23 01:18:04 -0300

    Personalized

--- quickstart.py
+++ quickstart.py
@@ -1,11 +1,17 @@
 """ Quickstart script for InstaPy usage """
 # imports
+import json
+import time
+import math
+import random
 from instapy import InstaPy
 from instapy.util import smart_run
 
 # login credentials
-insta_username = ''
-insta_password = ''
+with open("credentials.json") as f:
+    credentials = json.load(f)
+insta_username = credentials["username"]
+insta_password = credentials["password"]
 
 # get an InstaPy session!
 # set headless_browser=True to run InstaPy in the background
@@ -16,14 +22,22 @@ session = InstaPy(username=insta_username,
 with smart_run(session):
     """ Activity flow """
     # general settings
+    max_follow = 300  # Amount of people to follow
+
     session.set_relationship_bounds(enabled=True,
                                     delimit_by_numbers=True,
                                     max_followers=4590,
                                     min_followers=45,
                                     min_following=77)
-
-    session.set_dont_include(["friend1", "friend2", "friend3"])
-    session.set_dont_like(["pizza", "#store"])
+    session.set_do_follow(enabled=True, percentage=10, times=1)
 
     # activity
-    session.like_by_tags(["natgeo"], amount=10)
+    session.unfollow_users(amount=300, InstapyFollowed=(
+        True, "all"), style="RANDOM", unfollow_after=48 * 60 * 60, sleep_delay=600)
+    tags = ["dji", "gopro", "drone"]
+    max_tags = 10  # Max number of total tags to get
+    tag_limit = math.ceil(max_tags / len(tags))
+    session.set_smart_hashtags(
+        tags, limit=tag_limit, sort='top', log_tags=True)
+    likes_per_tag = int(max_follow * 10 / max_tags)
+    session.like_by_tags(amount=likes_per_tag, use_smart_hashtags=True)
